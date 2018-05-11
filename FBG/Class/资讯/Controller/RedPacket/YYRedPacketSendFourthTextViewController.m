7052c8d156b8820a34e99273d9b0fbe7e20f35c3
yy312 <413646278@qq.com>
2018-05-11 12:28:56 +0800

    删除的

--- FBG/Class/资讯/Controller/RedPacket/YYRedPacketSendFourthTextViewController.m
+++ FBG/Class/资讯/Controller/RedPacket/YYRedPacketSendFourthTextViewController.m
@@ -8,8 +8,9 @@
 
 #import "YYRedPacketSendFourthTextViewController.h"
 #import "DBHPlaceHolderTextView.h"
+#import "YYSharePromptView.h"
 
-@interface YYRedPacketSendFourthTextViewController ()
+@interface YYRedPacketSendFourthTextViewController ()<UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *progressView;
 @property (weak, nonatomic) IBOutlet UILabel *fourthLabel;
@@ -23,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UIView *bestView;
 @property (weak, nonatomic) IBOutlet DBHPlaceHolderTextView *bestTextView;
 @property (weak, nonatomic) IBOutlet UIButton *shareBtn;
+@property (assign, nonatomic) BOOL isSendRedBag;
 
 @end
 
@@ -48,7 +50,10 @@
 
 #pragma mark ------- SetUI ---------
 - (void)setUI {
-    self.title = DBHGetStringWithKeyFromTable(@"Send RedPacket", nil);
+    self.title = DBHGetStringWithKeyFromTable(@"Send Red Packet", nil);
+    
+    [self.senderNameTextField addTarget:self action:@selector(textFieldTextChange:) forControlEvents:UIControlEventEditingChanged];
+    self.bestTextView.delegate = self;
     
     CALayer *layer = [CALayer layer];
     layer.frame = CGRectMake(0, 0, SCREEN_WIDTH, 4);
@@ -63,22 +68,97 @@
     [self.senderView setBorderWidth:0.5f color:COLORFROM16(0xD9D9D9, 1)];
     [self.bestView setBorderWidth:0.5f color:COLORFROM16(0xD9D9D9, 1)];
     
-    self.senderNameTextField.placeholder = DBHGetStringWithKeyFromTable(@"Sender Name", nil);
-    self.bestTextView.placeholder = DBHGetStringWithKeyFromTable(@"Best / Message", nil);
+    self.senderNameTextField.placeholder = DBHGetStringWithKeyFromTable(@"Sender's Name", nil);
+    self.bestTextView.placeholder = DBHGetStringWithKeyFromTable(@"Wishes / Messages", nil);
+    
+    [self.shareBtn setBackgroundColor:COLORFROM16(0xD5D5D5, 1) forState:UIControlStateDisabled];
+    [self.shareBtn setBackgroundColor:COLORFROM16(0xEA6204, 1) forState:UIControlStateNormal];
     
     [self.shareBtn setCorner:2];
    
     [self.shareBtn setTitle:DBHGetStringWithKeyFromTable(@" Share ", nil) forState:UIControlStateNormal];
+    
+    self.shareBtn.enabled = NO;
+}
+
+#pragma mark ------- Data ---------
+- (void)sendRedPacket:(NSString *)senderName best:(NSString *)best {
+    dispatch_async(dispatch_get_global_queue(
+                                             DISPATCH_QUEUE_PRIORITY_DEFAULT,
+                                             0), ^{
+        NSString *urlStr = [NSString stringWithFormat:@"redbag/send/%@/%@", @(self.model.redPacketId), self.model.redbag_addr];
+        
+        NSDictionary *params = @{
+                                 @"share_type" : @"2", // 红包分享类型,1.图片,2.文字,3.url,4code
+                                 @"share_attr" : @"", // 红包分享内容,图片链接,文章内容,url
+                                 @"share_user" : senderName, // 红包分享用户
+                                 @"share_msg" : best, // 红包分享消息
+                                 };
+        WEAKSELF
+        [PPNetworkHelper POST:urlStr baseUrlType:3 parameters:params hudString:DBHGetStringWithKeyFromTable(@"Loading...", nil) success:^(id responseObject) {
+            [weakSelf handleResponse:responseObject];
+        } failure:^(NSString *error) {
+            [LCProgressHUD showFailure:DBHGetStringWithKeyFromTable(@"Load failed", nil)];
+        }];
+    });
+}
+
+- (void)handleResponse:(id)responseObj {
+    if ([NSObject isNulllWithObject:responseObj]) {
+        return;
+    }
+    
+    if ([responseObj isKindOfClass:[NSDictionary class]]) {
+        _isSendRedBag = YES;
+        self.backIndex = 2;
+        
+        YYRedPacketDetailModel *model = [YYRedPacketDetailModel mj_objectWithKeyValues:responseObj];
+        
+        YYSharePromptView *shareView = [[YYSharePromptView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        shareView.model = model;
+        
+        [[UIApplication sharedApplication].keyWindow addSubview:shareView];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [shareView show];
+        });
+    }
+}
+
+
+#pragma mark ------- text field and text view ---------
+- (void)textFieldTextChange:(UITextField *)textField {
+    if (textField.text.length != 0 && self.bestTextView.text.length != 0) {
+        self.shareBtn.enabled = YES;
+    } else {
+        self.shareBtn.enabled = NO;
+    }
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    if (textView.text.length != 0 && self.senderNameTextField.text.length != 0) {
+        self.shareBtn.enabled = YES;
+    } else {
+        self.shareBtn.enabled = NO;
+    }
 }
 
 #pragma mark ----- RespondsToSelector ---------
 - (IBAction)respondsToShareBtn:(UIButton *)sender {
-    NSString *bestStr = self.bestTextView.text;
-    if (bestStr.length > 0 && bestStr.length < 10) {
-        //YYTODO
-//        return;
+    if (_isSendRedBag) {
+        return;
     }
     
+    NSString *bestStr = self.bestTextView.text;
+    NSString *senderName = self.senderNameTextField.text;
     
+    if ([NSObject isNulllWithObject:bestStr]) {
+        bestStr = @"";
+    }
+    
+    if ([NSObject isNulllWithObject:senderName]) {
+        senderName = @"";
+    }
+    [self sendRedPacket:senderName best:bestStr];
 }
+
 @end
--- FBG/Class/资讯/Model/RedPacket/YYRedPacketDetailModel.h
+++ FBG/Class/资讯/Model/RedPacket/YYRedPacketDetailModel.h
@@ -10,35 +10,42 @@
 
 @interface YYRedPacketDetailModel : NSObject
 
-@property (nonatomic, assign) NSInteger modelId;
-@property (nonatomic, copy) NSString *auth_tx_id;
-@property (nonatomic, copy) NSString *redbag_tx_id;
-@property (nonatomic, assign) NSInteger redbag_id;
-@property (nonatomic, copy) NSString *redbag;
-@property (nonatomic, copy) NSString *redbag_symbol; // 红包代币
-@property (nonatomic, copy) NSString *redbag_addr;
-@property (nonatomic, assign) NSInteger redbag_number;
-@property (nonatomic, copy) NSString *fee;
-@property (nonatomic, copy) NSString *fee_addr;
+@property (nonatomic, assign) NSInteger redPacketId;
+@property (nonatomic, copy) NSString *auth_tx_id; // 授权tx_id
+@property (nonatomic, copy) NSString *redbag_tx_id; // 发送红包tx_id
+@property (nonatomic, copy) NSString *fee_tx_id; // 手续费tx_id
+@property (nonatomic, assign) NSInteger redbag_id;// 合约红包ID
+@property (nonatomic, copy) NSString *redbag; // 红包金额
+@property (nonatomic, copy) NSString *redbag_symbol; // 红包代币
+@property (nonatomic, copy) NSString *redbag_addr; // 发红包钱包地址
+@property (nonatomic, assign) NSInteger redbag_number; // 红包数量
+@property (nonatomic, copy) NSString *fee; // 手续费
+@property (nonatomic, copy) NSString *fee_addr; // 手续费地址
 @property (nonatomic, assign) NSInteger share_type;
 @property (nonatomic, copy) NSString *share_attr;
 @property (nonatomic, copy) NSString *share_user;
 @property (nonatomic, copy) NSString *share_msg;
-@property (nonatomic, copy) NSString *created_at;
+@property (nonatomic, copy) NSString *created_at; // 红包创建时间
 @property (nonatomic, copy) NSString *updated_at;
 @property (nonatomic, copy) NSString *share_theme_url;
-@property (nonatomic, copy) NSString *fee_tx_id;
-@property (nonatomic, assign) BOOL done; // 红包是否开奖,1.已开奖,0.待开奖
-@property (nonatomic, assign) RedBagStatus status; // 红包状态,1.完成,2.礼金打包,3.红包创建中,4.领取中, -2.礼金打包失败(授权失败),-3.红包创建失败
+@property (nonatomic, assign) RedBagLotteryStatus done; // 红包是否完成
+@property (nonatomic, assign) RedBagStatus status; // 红包状态,1.完成,2.礼金打包,3.红包创建中,4.领取中,-2.礼金打包失败(授权失败),-3.红包创建失败,-202 礼包授权pending中, -303.红包创建pending中
 @property (nonatomic, assign) NSInteger draw_redbag_number; // 红包已领取个数
 @property (nonatomic, assign) NSInteger auth_block; // 授权块高
 @property (nonatomic, assign) NSInteger redbag_block; // 红包块高
 @property (nonatomic, assign) NSInteger fee_block; // 手续费块高
 @property (nonatomic, assign) NSInteger redbag_back_block; // 红包退回块高
+
 @property (nonatomic, copy) NSString *redbag_back; // 红包退回金额
 @property (nonatomic, copy) NSString *redbag_back_tx_id; // 红包退回tx_id
-@property (nonatomic, strong) YYRedPacketRedBagInfoModel *redbag_info;
+@property (nonatomic, copy) NSString *auth_at; // 授权时间
+@property (nonatomic, copy) NSString *redbag_at; // 创建红包时间
+@property (nonatomic, copy) NSString *redbag_back_at; // 红包退回时间
+@property (nonatomic, assign) RedBagTXStatus redbag_back_tx_status;// 红包退回状态
+@property (nonatomic, strong) YYRedPacketEthTokenModel *gnt_category; // 红包发送情况
+
+//@property (nonatomic, strong) YYRedPacketRedBagInfoModel *redbag_info;
+
 @property (nonatomic, strong) NSArray *draws;
-@property (nonatomic, strong) YYRedPacketOpenedRedBagGntCategoryModel *gnt_category; // 红包发送情况
 
 @end
